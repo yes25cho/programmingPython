8ed7381302e6deff27c0ec2bb09107a8360bcfee
yscho010612@gmail.com <yscho010612@gmail.com>
2022-03-22 10:15:50 +0900

    변수 초기화, 변수 출력 하는 방법

--- 20220322_helloworld.py
+++ 20220322_helloworld.py
@@ -1,5 +1,10 @@
 print("Hello, World")
-print('Hello,조예서!')
+name = '양다연'
+print('Hello, '+name+'!')
+print('hello %s!'%name)
+print('Hello, {}! '.format(name))
+print(f'hello {name}!')
+
 #큰따("") 작따('') 같음
 #작따('')출력 \'
 print("Red velvet's favorite song is Red flavour.")
