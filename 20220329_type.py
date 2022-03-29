7284d965a7f9073a75a96438d31f7476ba8aabc6
yscho010612@gmail.com <yscho010612@gmail.com>
2022-03-29 10:18:13 +0900

    변수, 숫자, int, float, str, \n, type(), //: 몫, %:나머지

---
+++ 20220329_type.py
@@ -0,0 +1,25 @@
+# 숫자
+student_number = 2214
+age = 18
+height = 165.4
+
+# 문자
+name = '임정훈'
+
+print(f'학번 : {student_number}\n이름 : {name}\n나이 : {age}\n키 : {height}')  # 학번 : 2214
+
+print(type(student_number))  # <class 'int'>
+print(type(name))  # <class 'str'>
+print(type(age))  # <class 'int'>
+print(type(height))  # <class 'float'>
+print(type(10.27))  # <class 'int'>
+print(type('1027'))  # <class 'str'>
+print(10 / 27)  # java : 0, python : 0.37037037037037035
+print(27 / 10)  # java : 2, python : 2.7
+print(27 // 10)  # python : 나눈 몫 : 2
+print(27 % 10)  # python : 나눈 몫 : 7
+print(type(10 / 27))  # <class 'float'>
+print(type(27//5))   # <class 'int'>
+print(type(27 % 10))   # <class 'int'>
+print(20 / 10)   # python : 나눈 몫 : 2.0
+print(type(20 / 10))   # <class 'float'>
