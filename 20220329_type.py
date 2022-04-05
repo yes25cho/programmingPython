fd3276855f8a1bf35c78b03a35b810865972e84c
yscho010612@gmail.com <yscho010612@gmail.com>
2022-04-05 09:50:44 +0900

    str(), int(), float(), +연산자, *연산자

--- 20220329_type.py
+++ 20220329_type.py
@@ -8,6 +8,7 @@ name = '임정훈'
 
 print(f'학번 : {student_number}\n이름 : {name}\n나이 : {age}\n키 : {height}')  # 학번 : 2214
 
+
 print(type(student_number))  # <class 'int'>
 print(type(name))  # <class 'str'>
 print(type(age))  # <class 'int'>
@@ -23,3 +24,25 @@ print(type(27//5))   # <class 'int'>
 print(type(27 % 10))   # <class 'int'>
 print(20 / 10)   # python : 나눈 몫 : 2.0
 print(type(20 / 10))   # <class 'float'>
+
+#변수 이름 규칙, 자료형변환
+#my_mbti, my_function(), MyClass #myMbti : camel-case, my_mbti : snake-case
+
+my_mbti = 'INTP'
+
+print(f'My MBTI : {my_mbti}')
+
+#print('My MBTI'+my_mbti+'age : '+age)   #java : String.toString(age); python: str(age)
+print('My MBTI'+my_mbti+', age : '+str(age))
+height = '165.2'
+print(float(height) + 10)   #java : Float.parseFloat(height);, python : float(height)
+
+#str(), int(), float(): 자료형변환 함수
+#+연산자 : 숫자 숫자 : 덧셈, 문자+문자 => 문자문자
+print(18 + 2)   #20
+print('18'+'2') #'182'
+print(18 * 2)   #36
+print('2' * 3)  #'2222'
+
+#짝을 10번 출력
+print('짝'*10)
